2254cc1745252bd5243c84ab01e6b3de0596efd6
Lucas Curry <currylw@mail.uc.edu>
2024-04-18 16:41:09 -0400

    LeetCode Problem 2 Solution

--- group5_FinalProject/LeetCodeProblem2.aspx
+++ group5_FinalProject/LeetCodeProblem2.aspx
@@ -1,17 +1,36 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LeetCodeProblem2.aspx.cs" Inherits="group5_FinalProject.LeetCodeProblem2" %>
+﻿<!--# Name: Lucas Curry
+# email: currylw@mail.uc.edu
+# Assignment Title: Final project
+# Due Date: 04/23
+# Course: IS 3050
+# Semester/Year: Spring 2024
+# Brief Description: This code is the front code of the leetcode problem
+# Citations: openai.com
+# Anything else that's relevant: -->
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LeetCodeProblem2.aspx.cs" Inherits="group5_FinalProject.LeetCodeProblem2" %>
 
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>LeetCode Problem 2</title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
-            <asp:Button ID="Button1" runat="server" Text="Button" />
+            <h1>LeetCode Problem 2 Description</h1>
+            <p>
+                The problem asks you to add two single digit numbers stored in reverse order and return them as a linked list. The numbers can include zero, but have no leading zeroes.
+            </p>
+            <h2>Test Case</h2>
+            <p>
+                Input: <br />
+                List 1: 2 -> 4 -> 3<br />
+                List 2: 5 -> 6 -> 4<br />
+            </p>
+            <h3>Solution:</h3>
+            <asp:Literal ID="SolutionLiteral" runat="server"></asp:Literal>
          </div>
-
     </form>
 </body>
 </html>
--- group5_FinalProject/LeetCodeProblem2.aspx.cs
+++ group5_FinalProject/LeetCodeProblem2.aspx.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/*# Name: Lucas Curry
+# email: currylw@mail.uc.edu
+# Assignment Title: Final project
+# Due Date: 04/23
+# Course: IS 3050
+# Semester/Year: Spring 2024
+# Brief Description: This part of the project is a code behind where the actual code from leetcode is
+# Citations: openai.com
+# Anything else that's relevant: */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +21,69 @@ namespace group5_FinalProject
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            ListNode l1 = new ListNode(2);
+            l1.next = new ListNode(4);
+            l1.next.next = new ListNode(3);
 
+            ListNode l2 = new ListNode(5);
+            l2.next = new ListNode(6);
+            l2.next.next = new ListNode(4);
+
+            ListNode result = AddTwoNumbers(l1, l2);
+
+            // Print the result (assuming you have a method to display it)
+            DisplayListNode(result);
+        }
+
+        // Define ListNode class (assuming you haven't defined it already)
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        // AddTwoNumbers method
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode dummyHead = new ListNode(0);
+            ListNode p = l1, q = l2, curr = dummyHead;
+            int carry = 0;
+
+            while (p != null || q != null)
+            {
+                int x = (p != null) ? p.val : 0;
+                int y = (q != null) ? q.val : 0;
+                int sum = carry + x + y;
+                carry = sum / 10;
+                curr.next = new ListNode(sum % 10);
+                curr = curr.next;
+                if (p != null) p = p.next;
+                if (q != null) q = q.next;
+            }
+
+            if (carry > 0)
+            {
+                curr.next = new ListNode(carry);
+            }
+
+            return dummyHead.next;
+        }
+
+        // Method to display ListNode (assuming you have a method to display it)
+        private void DisplayListNode(ListNode node)
+        {
+            string result = "";
+            while (node != null)
+            {
+                result += node.val + " ";
+                node = node.next;
+            }
+            SolutionLiteral.Text = "Solution: " + result;
         }
     }
 }
